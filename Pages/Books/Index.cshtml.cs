ec3f7e9a8c34a021b40fa79edccec30991958df8
Radu Ciurca <raduciurca@gmail.com>
2023-10-30 15:14:15 +0200

    laborator 4 fara sarcina

--- Pages/Books/Index.cshtml.cs
+++ Pages/Books/Index.cshtml.cs
@@ -19,12 +19,15 @@ namespace Ciurca_Radu_Lab2.Pages.Books
         public int CategoryID { get; set; }
         public string TitleSort { get; set; }
         public string AuthorSort { get; set; }
-        public async Task OnGetAsync(int? id, int? categoryID, string sortOrder)
+        public string CurrentFilter {  get; set; }
+        public async Task OnGetAsync(int? id, int? categoryID, string sortOrder, string searchString)
         {
             BookD = new BookData();
 
             TitleSort = String.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
             AuthorSort = sortOrder == "author" ? "author_desc" : "author";
+
+            CurrentFilter = searchString;
 
             BookD.Books = await _context.Book
             .Include(b => b.Publisher)
@@ -34,6 +37,13 @@ namespace Ciurca_Radu_Lab2.Pages.Books
             .AsNoTracking()
             .OrderBy(b => b.Title)
             .ToListAsync();
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                BookD.Books = BookD.Books.Where(s => s.Author.FirstName.Contains(searchString)
+
+               || s.Author.LastName.Contains(searchString)
+               || s.Title.Contains(searchString));
+            }
             if (id != null)
             {
                 BookID = id.Value;
