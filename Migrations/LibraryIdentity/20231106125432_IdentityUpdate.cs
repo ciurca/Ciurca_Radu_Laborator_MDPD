352c84cd89d7d062ac6b4da1bede7045b50723ac
Radu Ciurca <raduciurca@gmail.com>
2023-11-06 16:19:07 +0200

    Integrare libarie Identity

---
+++ Migrations/LibraryIdentity/20231106125432_IdentityUpdate.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ciurca_Radu_Lab2.Migrations.LibraryIdentity
+{
+    public partial class IdentityUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
